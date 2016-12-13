718340f25b2b28f7c1f657b92a6bf48db447daee
lemonruan <limeng.ruan@stonybrook.edu>
2016-12-12 19:36:10 -0500

    add mouse moving camera

---
+++ TechWar/UnityProject/TechCompanyWar/Assets/GameScripts/TutorialTextScript.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TutorialTextScript : MonoBehaviour {
+
+    public Button Text1;
+    public Button Text2;
+    public Button Text3;
+    public Button Text4;
+    public Button Text5;
+    public Button Text6;
+    public Button Text7;
+
+    // Use this for initialization
+    void Start () {
+        //Text1.gameObject.SetActive(false);
+        Text2.gameObject.SetActive(false);
+        Text3.gameObject.SetActive(false);
+        Text4.gameObject.SetActive(false);
+        Text5.gameObject.SetActive(false);
+        Text6.gameObject.SetActive(false);
+        Text7.gameObject.SetActive(false);
+    }
+
+    public void goToNextText()
+    {
+        if (Text1.gameObject.activeSelf)
+        {
+            Text2.gameObject.SetActive(true);
+            Text1.gameObject.SetActive(false);
+        } else if (Text2.gameObject.activeSelf)
+        {
+            Text3.gameObject.SetActive(true);
+            Text2.gameObject.SetActive(false);
+        } else if (Text3.gameObject.activeSelf)
+        {
+            Text4.gameObject.SetActive(true);
+            Text3.gameObject.SetActive(false);
+        }
+        else if (Text4.gameObject.activeSelf)
+        {
+            Text5.gameObject.SetActive(true);
+            Text4.gameObject.SetActive(false);
+        }
+        else if (Text5.gameObject.activeSelf)
+        {
+            Text6.gameObject.SetActive(true);
+            Text5.gameObject.SetActive(false);
+        }
+        else if (Text6.gameObject.activeSelf)
+        {
+            Text7.gameObject.SetActive(true);
+            Text6.gameObject.SetActive(false);
+        } else if (Text7.gameObject.activeSelf)
+        {
+
+            Text7.gameObject.SetActive(false);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- TechWar/UnityProject/TechCompanyWar/Assets/GameScripts/cameraScript.cs
+++ TechWar/UnityProject/TechCompanyWar/Assets/GameScripts/cameraScript.cs
@@ -4,11 +4,16 @@ using System.Collections;
 public class cameraScript : MonoBehaviour {
     public float speed;
     public float height;
+
+    //3 for up, 4 for down, 2 for left, 1 for right
+    int move;
+
 	// Use this for initialization
 	void Start () {
         var pos = GetComponent<Transform>().position;
         pos.y = height;
         GetComponent<Transform>().position = pos;
+        move = 0;
 	}
 	
 	// Update is called once per frame
@@ -18,5 +23,31 @@ public class cameraScript : MonoBehaviour {
             Input.GetAxis("Vertical") * speed * Time.deltaTime,
             0
             );
+        if (move > 0)
+        {
+            if(move == 1)
+            {
+                transform.Translate(1*speed*Time.deltaTime, 0,0);
+            }else if(move == 2)
+            {
+                transform.Translate((-1) * speed * Time.deltaTime, 0, 0);
+            }else if(move == 3)
+            {
+                transform.Translate(0,1 * speed * Time.deltaTime, 0);
+            }else if(move == 4)
+            {
+                transform.Translate(0, (-1) * speed * Time.deltaTime, 0);
+            }
+        }
 	}
+
+    public void moveCamera(int direction)
+    {
+        move = direction;
+    }
+
+    public void MoveStop()
+    {
+        move = 0;
+    }
 }
